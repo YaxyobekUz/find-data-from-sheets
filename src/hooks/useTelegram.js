1761cddb492b741db5feec8217980014ee1ffd59
yaxyobekb <pubgn9642@gmail.com>
2024-12-06 08:39:45 +0500

    add telegram web app scripts

---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export default () => {
+  const tg = window.Telegram.WebApp;
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const openTelegramLink = (url) => {
+    tg.openTelegramLink(`https://t.me/${url}`);
+  };
+
+  const user = tg?.initDataUnsafe?.user;
+
+  return { onClose, user, tg, openTelegramLink };
+};
